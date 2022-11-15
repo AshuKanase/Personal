4714e8e2dc3527031252c6c897867dcfb7a6f9d4
Acer <Acer@DESKTOP-17F2MQ1>
2022-11-15 16:17:58 +0530

    hi

--- src/com/StacN/java/project1/NewClass.java
+++ src/com/StacN/java/project1/NewClass.java
@@ -1,5 +1,10 @@
 package com.StacN.java.project1;
 
-public class NewClass {
+public class NewClass 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("This is my push code");
+	}
 
 }
